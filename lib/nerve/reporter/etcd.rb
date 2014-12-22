c34c5f4f073d893adce30253562beb87f3f50445
Konstantin Krauss <konstantin@propertybase.com>
2014-12-22 17:15:10 +0100

    Fix @full_key bug

--- lib/nerve/reporter/etcd.rb
+++ lib/nerve/reporter/etcd.rb
@@ -67,7 +67,7 @@ class Nerve::Reporter
     def etcd_save
       return etcd_create unless @full_key
       begin
-        @etcd.set(@key, :value => @data, :ttl => 30)
+        @etcd.set(@full_key, :value => @data, :ttl => 30)
       rescue ::Etcd::KeyNotFound
         etcd_create
       end
